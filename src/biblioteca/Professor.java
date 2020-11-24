6054ded87aee881a17b75faf3b3efb5affd687fc
Andressa Amorim <dev.andymal@gmail.com>
2020-11-24 15:11:57 -0300

    adicionando classe professor

--- src/biblioteca/Professor.java
+++ src/biblioteca/Professor.java
@@ -1,5 +1,35 @@
 package biblioteca;
 
-public class Professor {
+public class Professor extends Pessoa{
+	
+	private String materia;
+	
+	private String registroProfessor;
+	
+	//super
+
+	public Professor(String materia, String registroProfessor) {
+		super();
+		this.materia = materia;
+		this.registroProfessor = registroProfessor;
+	}
+	
+	public String getMateria() {
+		return materia;
+	}
+
+	public void setMateria(String materia) {
+		this.materia = materia;
+	}
+
+	public String getRegistroProfessor() {
+		return registroProfessor;
+	}
+
+	public void setRegistroProfessor(String registroProfessor) {
+		this.registroProfessor = registroProfessor;
+	}
+	
+	
 
 }
