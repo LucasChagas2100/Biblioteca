9a8f0d84dda4ce612101cdf21b1e850ff82dedd5
Isabelle <isa_ozawa@hotmail.com>
2020-11-24 15:17:03 -0300

    Classe aluno

--- src/biblioteca/Aluno.java
+++ src/biblioteca/Aluno.java
@@ -1,5 +1,38 @@
 package biblioteca;
 
-public class Aluno {
+public class Aluno extends Pessoa {
+	
+	public Aluno() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	private long registroAluno;
+	private int serie;
+	
+	public Aluno(long registroAluno, int serie) {
+		super();
+		this.registroAluno = registroAluno;
+		this.serie = serie;
+	}
+
+	public long getRegistroAluno() {
+		return registroAluno;
+	}
+
+	public void setRegistroAluno(long registroAluno) {
+		this.registroAluno = registroAluno;
+	}
+
+	public int getSerie() {
+		return serie;
+	}
+
+	public void setSerie(int serie) {
+		this.serie = serie;
+	}
+	
+	
+	
 
 }
